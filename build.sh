13dfce9ff4dfc3fee074359dfe8f3679293e2e34
Cheng Shao <terrorjack@type.dance>
2024-05-16 17:50:39 +0000

    v22.2.0

--- build.sh
+++ build.sh
@@ -2,27 +2,20 @@
 
 set -eu
 
-node_ver=v22.1.0
+node_ver=v22.2.0
 
 apk add \
-  clang \
   linux-headers \
-  lld \
-  llvm \
   python3 \
   xz
 
 cd "$(mktemp -d)"
 
-curl -f -L --retry 5 https://nodejs.org/dist/$node_ver/node-$node_ver.tar.gz | tar xz --strip-components=1
-patch -p1 -i /workspace/node-clang-lto.diff
+curl -f -L --retry 5 https://nodejs.org/dist/$node_ver/node-$node_ver.tar.xz | tar xJ --strip-components=1
 patch -p1 -i /workspace/use-etc-ssl-certs.patch
 
 make -j"$(nproc)" binary \
-  AR="llvm-ar" \
-  CC="clang" \
-  CXX="clang++" \
-  CONFIG_FLAGS="--enable-lto --experimental-enable-pointer-compression --fully-static --openssl-use-def-ca-store" \
+  CONFIG_FLAGS="--experimental-enable-pointer-compression --fully-static --openssl-use-def-ca-store" \
   VARIATION="static"
 
 mv node-$node_ver-linux-x64-static.tar.xz /workspace
