7e2671fed4ea019bfd50e1eca81be0f6eb2ae076
Cheng Shao <terrorjack@type.dance>
2024-12-19 01:02:00 +0000

    v23.5.0

--- build-alpine.sh
+++ build-alpine.sh
@@ -2,10 +2,12 @@
 
 set -eu
 
-node_ver=v23.4.0
+node_ver=v23.5.0
 
 apk add \
+  clang \
   linux-headers \
+  llvm \
   python3 \
   xz
 
@@ -13,10 +15,14 @@ cd "$(mktemp -d)"
 
 curl -f -L --retry 5 https://nodejs.org/dist/$node_ver/node-$node_ver.tar.xz | tar xJ --strip-components=1
 patch -p1 -i /workspace/bump-v8-wasm-limits.diff
+patch -p1 -i /workspace/lto.diff
 patch -p1 -i /workspace/use-etc-ssl-certs.diff
 
 make -j"$(nproc)" binary \
-  CONFIG_FLAGS="--fully-static --openssl-use-def-ca-store --v8-disable-maglev" \
+  AR=llvm-ar \
+  CC=clang \
+  CXX=clang++ \
+  CONFIG_FLAGS="--enable-lto --fully-static --openssl-use-def-ca-store --v8-disable-maglev" \
   VARIATION="static"
 
 mkdir /workspace/dist
