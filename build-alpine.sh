0a19748190848d860de770c683de5a7488e27cb6
Cheng Shao <terrorjack@type.dance>
2026-03-04 15:17:41 +0100

    v25.8.0

--- build-alpine.sh
+++ build-alpine.sh
@@ -2,7 +2,7 @@
 
 set -eu
 
-node_ver=v25.7.0
+node_ver=v25.8.0
 
 apk add \
   clang \
--- utils/update-diff.sh
+++ utils/update-diff.sh
@@ -7,7 +7,7 @@ trap 'rm -rf "$workdir"' EXIT
 
 cd "$workdir"
 
-git clone --depth=1 --branch=v25.7.0 https://github.com/nodejs/node.git .
+git clone --depth=1 --branch=v25.8.0 https://github.com/nodejs/node.git .
 
 git apply "$OLDPWD/lto.diff"
 
